e91e13cc3c4304936c34926dba15a08f6c3fa6a0
lovepreet4313 <lovisaini3@gmail.com>
2025-02-12 22:38:50 +0530

    initial commit

---
+++ Chapter 3 - PS/03_problem3.py
@@ -0,0 +1,5 @@
+# Write a program to detect double space in a string
+
+name = "Lovi is a good  boy"
+
+print(name.find("  "))
---
+++ Chapter 3 - PS/04_problem4.py
@@ -0,0 +1,6 @@
+# Replace the double space from problem 3 with single spaces.
+
+name = "Lovi is a good  boy"
+
+print(name.replace("  ", " "))
+print(name) # Strings are immutable which means that we cannot change them by running functions on them
