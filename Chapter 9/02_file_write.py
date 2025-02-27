60a31865412ebec67a3c9aea5f031b83cb2a0481
lovepreet4313 <lovisaini3@gmail.com>
2025-02-27 20:59:15 +0530

    initial commit

---
+++ Chapter 9/02_file_write.py
@@ -0,0 +1,6 @@
+st = "Lovepreet is a good DevOps Engineer"
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+f.close()
---
+++ Chapter 9/04_append.py
@@ -0,0 +1,6 @@
+st = "Messi is a good dribbler"
+
+f = open("myfile.txt", "a")
+
+f.write(st + "\n")
+f.close()
